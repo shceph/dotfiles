3f0893378d024cc55a6b45805449bbcd1a4a54f1
shceph <milenkovic.stefan1122@gmail.com>
2025-07-25 16:21:15 +0200

    waybar overhaul

--- hyprland/.config/hypr/start-hyprland.sh
+++ hyprland/.config/hypr/start-hyprland.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-export GTK_THEME=Adwaita:dark
+export XDG_CURRENT_DESKTOP=Hyprland
+export XDG_SESSION_TYPE=wayland
+export XDG_SESSION_DESKTOP=Hyprland
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
+export GDK_BACKEND=wayland,x11
+export GTK_THEME=Orchis-Dark
 exec Hyprland
--- nvim/.config/nvim/lua/config/plugins/nvim-dap.lua
+++ nvim/.config/nvim/lua/config/plugins/nvim-dap.lua
@@ -30,7 +30,7 @@ return {
 			local dap = require('dap')
 			dap.adapters.lldb = {
 				type = 'executable',
-				command = '/usr/bin/lldb-dap', -- adjust as needed, must be absolute path
+				command = '/usr/bin/codelldb', -- adjust as needed, must be absolute path
 				name = 'lldb'
 			}
 
